275554d1f62370646cf745804bd2f2e93fff3575
li.he <li.he@YumChina.com>
2018-05-08 11:19:39 +0800

    作业平台修改bug提交

--- devops_tools/views/devops_tools.py
+++ devops_tools/views/devops_tools.py
@@ -22,18 +22,11 @@ class DevopsToolsListView(LoginRequiredMixin, OrderableListMixin, ListView):
             tool_list_result = hu.get(serivceName="job", restName="/rest/job/list_tool_set/",datas=reqData)
             tool_list = tool_list_result.get("results", {})
             count = tool_list_result.get("count", 0)
-            new_tool_list = []
             for tool in tool_list:
-                tool_set_prime_type = tool["tool_set_prime_type"]
-                tool_set_type = tool["tool_set_type"]
                 tool['param'] = json.loads(tool['param'])
-                if tool_set_prime_type == 1 and tool_set_type != 4 and tool_set_type != 6:
-                    new_tool_list.append(tool)
-                else:
-                    count -= 1
-            paginator = Paginator(new_tool_list, req.limit)
+            paginator = Paginator(tool_list, req.limit)
             paginator.count = count
-            context['result_list'] = new_tool_list
+            context['result_list'] = tool_list
             context['is_paginated'] = count > 0
             context['page_obj'] = paginator.page(req.offset)
             context['paginator'] = paginator
