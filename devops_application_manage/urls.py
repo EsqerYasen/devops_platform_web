487194ee5b1757f7047100613ff5199494e15d4d
li.he <li.he@YumChina.com>
2018-05-23 22:01:52 +0800

    作业平台修改bug提交

--- devops_application_manage/urls.py
+++ devops_application_manage/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from devops_application_manage.views.app_deploy import *
+from devops_application_manage.views.file_monitored import *
 
 urlpatterns = [
     url(r'^manage/', include([
@@ -9,5 +10,8 @@ urlpatterns = [
         url(r'^delete/', DevopsAppMgeDeleteView.as_view(), name='delete'),
         url(r'^(?P<pk>\d+)/deploy/', DevopsAppMgeDeployView.as_view(), name='deploy'),
         url(r'^get_command_set_info', GetCommandSetInfoView.as_view(), name='get_command_set_info'),
+        url(r'^filemonitoredlist', FileMonitoredView.as_view(), name='filemonitoredlist'),
+        url(r'^filemonitoredconfiglist', FileMonitoredConfigListView.as_view(), name='filemonitoredconfiglist'),
+        url(r'^filemonitoredconfigadd', FileMonitoredConfigCreateView.as_view(), name='filemonitoredconfigadd'),
     ]))
 ]
--- devops_application_manage/views/file_monitored.py
+++ devops_application_manage/views/file_monitored.py
@@ -1,19 +1,83 @@
 from braces.views import *
 from django.views.generic import *
+from common.utils.HttpUtils import *
+from django.core.paginator import Paginator
+from django.http import HttpResponse
 import logging
 
 logger = logging.getLogger('devops_platform_log')
 
-class List1View(LoginRequiredMixin, OrderableListMixin, ListView):
+class FileMonitoredView(LoginRequiredMixin, OrderableListMixin, ListView):
     template_name = "file_monitored_list.html"
 
     def get_queryset(self):
         return []
 
     def get_context_data(self, **kwargs):
-        context = super(List1View, self).get_context_data(**kwargs)
+        context = super(FileMonitoredView, self).get_context_data(**kwargs)
         try:
-            pass
+            hu = HttpUtils(self.request)
+            reqData = hu.getRequestParam()
+            inotify_result = hu.get(serivceName="cmdb", restName="/rest/inotify/list/",datas=reqData)
+            inotify_list = inotify_result.get("results", [])
+            context['result_list'] = list
+            count = inotify_result.get("count", 0)
+            paginator = Paginator(inotify_list, self.request.limit)
+            paginator.count = count
+            context['result_list'] = inotify_list
+            context['is_paginated'] = count > 0
+            context['page_obj'] = paginator.page(self.request.offset)
+            context['paginator'] = paginator
         except Exception as e:
             logging.error(e)
         return context
+
+
+class FileMonitoredConfigListView(LoginRequiredMixin, OrderableListMixin, ListView):
+    template_name = "file_monitored_config_list.html"
+
+    def get_queryset(self):
+        return []
+
+    def get_context_data(self, **kwargs):
+        context = super(FileMonitoredConfigListView, self).get_context_data(**kwargs)
+        try:
+            hu = HttpUtils(self.request)
+            reqData = hu.getRequestParam()
+            inotify_result = hu.get(serivceName="cmdb", restName="/rest/inotify/monitoritemlist/", datas=reqData)
+            inotify_list = inotify_result.get("results", [])
+            context['result_list'] = list
+            count = inotify_result.get("count", 0)
+            paginator = Paginator(inotify_list, self.request.limit)
+            paginator.count = count
+            context['result_list'] = inotify_list
+            context['is_paginated'] = count > 0
+            context['page_obj'] = paginator.page(self.request.offset)
+            context['paginator'] = paginator
+        except Exception as e:
+            logging.error(e)
+        return context
+
+class FileMonitoredConfigCreateView(LoginRequiredMixin, JSONResponseMixin,AjaxResponseMixin, TemplateView):
+    template_name = "file_monitored_config_add.html"
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        try:
+            hu = HttpUtils(self.request)
+            getData = {'offset': 0, 'limit': 1000, 'is_enabled': 1}
+            hostgroupResult = hu.get(serivceName="cmdb", restName="/rest/hostgroup/list_tree/", datas=getData)
+            context['hostGroup_list'] = hostgroupResult.get("data", [])
+        except Exception as e:
+            logger.error(e)
+        return context
+
+    def post_ajax(self, request, *args, **kwargs):
+        result = {'status': 0}
+        try:
+            pass
+        except Exception as e:
+            result['status'] = 1
+            result['msg'] = '保存异常'
+            logger.error(e)
+        return HttpResponse(json.dumps(result),content_type='application/json')
