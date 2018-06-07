8c629e023181323e04ecf5ac3f32b785a8aea6e1
li.he <li.he@YumChina.com>
2018-06-07 17:15:52 +0800

    作业平台修改bug提交

--- common_platform/views/command_set.py
+++ common_platform/views/command_set.py
@@ -409,7 +409,7 @@ class GetCommandExecuteLogView(LoginRequiredMixin,JSONResponseMixin, AjaxRespons
             log_str = ""
             if deploy_id and bind_type:
                 log_k = "%s_%s_log" % (deploy_id, bind_type)
-                r_v1 = RedisBase.get("%s_%s" % (deploy_id, bind_type), 1)
+                r_v1 = str(RedisBase.get("%s_%s" % (deploy_id, bind_type), 1),encoding="utf-8")
                 r_v2 = RedisBase.exists(log_k,1)
                 logger.info("r_v1:%s  r_v2:%s" %(r_v1,r_v2))
                 if r_v1 is not None or r_v2:
