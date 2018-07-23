be199ba1e5f6f64963fb44627774ae5c8153f324
eric <eric@localhost>
2018-07-23 13:16:51 +0800

    常用作业

---
+++ static/js_src/commandSet/view.js
@@ -0,0 +1,69 @@
+  var Backbone = require('backbone');
+  Backbone.$ = exports.$ = window.jQuery;
+  var _ = require('underscore');
+  window.YUMEVENTS = _.extend({}, Backbone.Events);
+  var gridTemplate = require('./templates/partials/grid.ejs');
+
+  var Screen = Backbone.View.extend({
+    el: '.wrapper-content',
+    userInfo: false,
+    model: false,
+    userId: false,
+    permssions: [],
+    events: {
+      'click .btn-primary.search': 'search',
+    },
+    initialize: function(user, userId, model) {
+      console.log('view inited');
+      this.userInfo = user;
+      this.model = model;
+      this.userId = userId;
+      $('.scrollable').scroll(this.onScroll.bind(this));
+      window.YUMEVENTS.on("loadmore", this.loadMore.bind(this));
+      this.showList();
+      $('.dataTables_wrapper').height(window.innerHeight - 200);
+    },
+    //var name = $('.btn-primary.search').val()
+    search: function(e){
+      this.name = $('input#name').val();
+      $('.command_set_list tbody').empty();
+      this.showList();
+      e.preventDefault();
+      e.stopPropagation();
+    },
+    showList: function(limit, offset) {
+
+      this.model.getList(this.userId, limit, offset, this.name).then(function(res) {
+        var model = this.model;
+        //{1:'常用命令',2:'上传文件',3:'远程文件',4:'shell脚本',5:'自定义'}
+        this.appendToGrid({
+          results: res.results
+        });
+      }.bind(this));
+    },
+    appendToGrid: function(param) {
+      var lists = param.results;
+      var versions = param.versions;
+      var permissions = param.permissions || [];
+      var val = 0;
+      if (lists.length) {
+        $.each(lists, function(i, item) {
+          $('.command_set_list tbody').append(gridTemplate({
+            item: item,
+            devopsToolsTypeTrans: {1:'常用命令',2:'上传文件',3:'远程文件',4:'shell脚本',5:'自定义'}
+          }));
+        }.bind(this))
+      }
+    },
+    loadMore: function() {
+      console.log("loadMore");
+      this.showList(10, $('tbody tr').length);
+    },
+    onScroll: function(e) {
+      if ($('.scrollable').scrollTop() + $('.scrollable').height() >= $('.scrollable .command_set_list').height()) {
+        console.log("bottom!");
+        this.loadMore();
+      }
+    }
+  })
+  module.exports = Screen;
