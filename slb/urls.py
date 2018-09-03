3b2472b9ad019f2811b11cc61ddd22a0a4807456
lch7395 <li.he@yumchina.com>
2018-09-03 11:48:28 +0800

    slb提交

--- slb/urls.py
+++ slb/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     url(r'^rest/configversiondiff/', site_views.ConfigVersionDiff.as_view(), name='configpreviewview'),
     url(r'^rest/configversiondiffview/', site_views.ConfigVersionDiffView.as_view(), name='configversiondiffview'),
 
-    url(r'^rest/mngsitecreateorupdate/', site_views.MngSiteCreateOrUpdateView.as_view(), name='mngsitecreate'),
+    #url(r'^rest/mngsitecreateorupdate/', site_views.MngSiteCreateOrUpdateView.as_view(), name='mngsitecreate'),
+    url(r'^rest/mngsitecreateorupdate/', views.MngSiteCreateOrUpdate, name='mngsitecreate'),
 ]
